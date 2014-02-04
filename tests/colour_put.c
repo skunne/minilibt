718a84433dbfc3d6381951dc871c74d2f65ef02f
avannest <dyarob@gmail.com>
2014-02-04 10:55:11 +0100

    test colour_put commented and improved

--- tests/colour_put.c
+++ tests/colour_put.c
@@ -5,25 +5,40 @@
 
 /*
 **  Testing how we use colors in termcaps.
+**
+** Results :
+** - tgoto : is actually a function to parse arguments
+**           to a capability.
+** - how we change background color : send a string like
+**           \E[48;5;COLOR_INDEXm //for 256 color mode
+** - " " " " " " " foreground color : " " " " " " " " "
+**           \E[38;5;COLOR_INDEXm //for 256 color mode
 */
 
 int				main(void)
 {
 	char		buff[1024];
-	char		*b;
+	char		buff2[1024];
 
 	tgetent(NULL, getenv("TERM"));
-	b = buff;
+
 	// On cherche a reproduire :
 	//write(1, tgoto(tgetstr("AB", NULL), 0, 169), 100);
 
 	strcpy(buff, tgoto(tgetstr("AB", NULL), 0, 17));
-	write(1, buff + 1, 100);
-	write(1, buff, 200);
+	write(1, buff + 1, 10);
+	write(1, buff, 20);
 
-	write(1, "attention on rechange!", 20);
-	write(1, "\E[48;5;177mbleuh", 20);
+	write(1, "\nattention on rechange!\n", 20);
+	write(1, "\E[48;5;199mbleuh\n", 20);
 
-	write(1, "bouh", 4);
+	strcpy(buff2, tgoto(tgetstr("AF", NULL), 0, 45));
+	write(1, buff2 + 1, 20);
+	write(1, buff2, 20);
+
+	write(1, "\nattention on rechange!\n", 20);
+	write(1, "\E[38;5;100mbleuh\n", 20);
+
+	write(1, "bouh\n", 4);
 	return (0);
 }
